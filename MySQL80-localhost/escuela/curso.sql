2bd3e12f1d3123193c9b90b7ec62406226da2ce8
Josue Terrazas Mendoza <terrazasjosue0@gmail.com>
2024-09-10 15:58:10 -0600

    ejercicio 1 | 10 - Sep

--- MySQL80-localhost/escuela/curso.sql
+++ MySQL80-localhost/escuela/curso.sql
@@ -1,5 +1,6 @@
+
 CREATE TABLE curso(
 id_curso INT PRIMARY KEY AUTO_INCREMENT,
 nombre VARCHAR(30) NOT NULL,
 creditos INT CHECK(creditos > 0), 
-)
+);
---
+++ MySQL80-localhost/escuela/inscripcion.sql
@@ -0,0 +1,7 @@
+
+CREATE TABLE inscripción(
+id_alumno INT,
+id_curso INT,
+PRIMARY KEY(id_alumno, id_curso),
+FOREIGN KEY()
+);
