ce139cb8ec19dccc82614908895f4745284566c7
Josue Terrazas Mendoza <terrazasjosue0@gmail.com>
2024-09-10 19:15:02 -0600

    ejercicio 2 | 10 - Sep

---
+++ MySQL80-localhost/tienda/cliente.sql
@@ -0,0 +1,7 @@
+
+CREATE TABLE cliente(
+id_cliente INT PRIMARY KEY AUTO_INCREMENT,
+nombre VARCHAR(30) NOT NULL,
+apellido1 VARCHAR(30) NOT NULL,
+apellido2 VARCHAR(30) NOT NULL
+);
---
+++ MySQL80-localhost/tienda/venta.sql
@@ -0,0 +1,4 @@
+CREATE TABLE venta(
+id_venta INT PRIMARY KEY AUTO_INCREMENT,
+fecha DATE NOT NULL
+);
